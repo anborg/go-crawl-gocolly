24ae96ae4e6942b78336b7871db34d996dd1118d
espre05 <espre05@gmail.com>
2020-05-08 19:06:23 -0400

    init

---
+++ elastic.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/elastic/go-elasticsearch/v8"
+)
+
+func main() {
+
+	// cert, _ := ioutil.ReadFile(*cacert)
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+			"http://localhost:9201",
+		},
+		// Username: "foo",
+		// Password: "bar",
+		// CACert: cert,
+		// Transport: &http.Transport{
+		// 	MaxIdleConnsPerHost:   10,
+		// 	ResponseHeaderTimeout: time.Second,
+		// 	TLSClientConfig: &tls.Config{
+		// 		MinVersion: tls.VersionTLS11,
+		// 		// ...
+		// 	},
+		// 	// ...
+		// }, //Transport
+	}
+
+	es, _ := elasticsearch.NewClient(cfg)
+
+	log.Println(elasticsearch.Version)
+	log.Println(es.Info())
+
+}
---
+++ oelastic.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"time"
+	"github.com/olivere/elastic/v7"
+)
+
+func main(){
+	client, err := elastic.NewClient(
+		elastic.SetURL("http://localhost:9200")
+		elastic.SetSniff(false),
+		elastic.SetHealthcheckInterval(10*time.Second),
+		elastic.SetRetrier(NewCustomRetrier()),
+		elastic.SetGzip(true),
+		elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),
+		elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags)),
+		elastic.SetHeaders(http.Header{
+		  "X-Caller-Id": []string{"..."},
+		}),
+	)
+	if err != nil {
+	// Handle error
+	panic(err)
+	}
+	defer client.Stop()
+
+	exists, err := client.IndexExists("websiteindex").Do(context.Background())
+	if err != nil {
+		// Handle error
+	}
+	if exists {
+		println("Index exists", "websiteindex")
+	}
+
+
+	// Create a new index.
+mapping := `{
+	"settings":{
+		"number_of_shards":1,
+		"number_of_replicas":0
+	},
+	"mappings":{
+		"properties":{
+			"url":{
+				"type":"keyword"
+			},
+			"title":{
+				"type":"geo_point"
+			},
+			"content":{
+				"type":"completion"
+			}
+		}
+	}
+}`
+
+ctx := context.Background()
+createIndex, err := client.CreateIndex("websiteindex").BodyString(mapping).Do(ctx)
+if err != nil {
+    // Handle error
+    panic(err)
+}
+if !createIndex.Acknowledged {
+    // Not acknowledged
+}
+
+}
