453b00a29242aca681720a20ad7b8e02312f3db6
anborg <dev1@gmail.com>
2020-05-07 21:38:09 -0400

    init

--- crawl.go
+++ crawl.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -17,8 +18,21 @@ type WebPage struct {
 }
 
 func main() {
+	var baseUrl string
 
-	baseUrl := "https://www.coursera.org"
+	flag.StringVar(&baseUrl, "url", "", "Crawl url is required")
+	flag.Parse()
+
+	if baseUrl == "" {
+		log.Println("crawl url required")
+		os.Exit(1)
+	}
+
+	webpage := WebPage{}
+	// baseUrl := "https://www.coursera.org"
+	// baseUrl := "https://www.markham.ca/wps/portal/home"
+
+	pageCount := 0
 
 	// q, _ := queue.New(
 	// 	2, // Number of consumer threads
@@ -26,30 +40,19 @@ func main() {
 	// )
 
 	c := colly.NewCollector(
-		// Visit only domains: coursera.org, www.coursera.org
-		colly.AllowedDomains("www.coursera.org"),
-
+		colly.AllowedDomains("www.markham.ca"),
 		//colly.AllowURLRevisit(),
-
-		// MaxDepth is 1, so only the links on the scraped page
-		// is visited, and no further links are followed
 		//colly.MaxDepth(1),
-
-		//#FORPARALLEL-code-anb
 		colly.Async(true),
-
-		// Attach a debugger to the collector
 		// colly.Debugger(&debug.LogDebugger{}), // "github.com/gocolly/colly/debug"
+		//colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
 	)
 	extensions.RandomUserAgent(c)
 	extensions.Referer(c)
 
-	//#FORPARALLEL-code-anb  http://go-colly.org/docs/examples/parallel/
-	// Limit the number of threads to 2
-	// when visiting links which domains' matches "*httpbin.*" glob
 	c.Limit(&colly.LimitRule{
 		DomainGlob:  "*",
-		Parallelism: 2,
+		Parallelism: 2, // http://go-colly.org/docs/examples/parallel/
 		//  RandomDelay: 5 * time.Second,
 		//Delay:      5 * time.Second,
 	})
@@ -60,66 +63,44 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	c.OnRequest(func(r *colly.Request) { // url
+		webpage.Url = r.URL.String()
+
+	})
+
+	c.OnResponse(func(r *colly.Response) { //get body
+		pageCount++
+		//webpage.Content = "page html body bla " // string(r.Body)
+		//urlVisited := r.Ctx.Get("url")
+		println(fmt.Sprintf("  DONE Visiting %d: %s", pageCount, webpage.Url))
+
+	})
+
+	c.OnHTML("html head title", func(e *colly.HTMLElement) { // Title
+		//e.Ctx.Put("title", e.Text)
+		webpage.Title = e.Text
+	})
+	c.OnHTML("html body", func(e *colly.HTMLElement) { // Body / content
+		//e.Ctx.Put("title", e.Text)
+		webpage.Content = "my body bla" // e.Text
+	})
+
+	// On every a element which has href attribute
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) { // href , callback
 		link := e.Attr("href")
-
-		// log.Println(fmt.Sprintf("\tTO CRAWL  : %s", link))
-		//Skip the following
-		//#, javascript:; ,  #
-
-		// start scaping the page under the link found
 		e.Request.Visit(link)
-
-		// Add URLs to the queue
-		// q.AddURL(link)
-
+		// q.AddURL(link) // Add URLs to the queue
 		//c.Visit(e.Request.AbsoluteURL(link))
 	})
 
-	c.OnHTML("title", func(e *colly.HTMLElement) {
-		//fmt.Println(e.Text)
-		//e.Ctx.Put("title", e.Text)
-	})
-
-	pageCount := 0
-	c.OnRequest(func(r *colly.Request) {
-
-		// log.Println(fmt.Sprintf("%d  Visiting : %s", pageCount, r.URL))
-
-		// Before making a request put the URL with
-		// the key of "url" into the context of the request
-		r.Ctx.Put("url", r.URL.String())
-	})
-
-	// Set error handler
-	c.OnError(func(r *colly.Response, err error) {
+	c.OnError(func(r *colly.Response, err error) { // Set error handler
 		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
 
-	// Print the response
-	c.OnResponse(func(r *colly.Response) {
-		pageCount++
-		//	log.Printf("%s\n", bytes.Replace(r.Body, []byte("\n"), nil, -1))
-		// log.Println(string(r.Body))
-		//log.Println(r.Headers)
-
-		// After making a request get "url" from
-		// the context of the request
-		urlVisited := r.Ctx.Get("url")
-		webpage := WebPage{
-			Url:     r.Ctx.Get("url"),      //- can be put in ctx c.OnRequest, and r.Ctx.Get("url")
-			Title:   "my page title",       //string(r.title), // Where to get this?
-			Content: "page html body bla ", //string(r.Body),   //string(r.Body) - can be done in c.OnResponse
-		}
-
+	c.OnScraped(func(r *colly.Response) { // DONE
 		enc := json.NewEncoder(os.Stdout)
 		enc.SetIndent("", "  ")
-		// Dump json to the standard output
 		enc.Encode(webpage)
-
-		log.Println(fmt.Sprintf("%d  DONE Visiting : %s", pageCount, urlVisited))
-
 	})
 
 	c.Visit(baseUrl)
